ee2cbab5d716e0fc454a1df09e021841baebe4d6
noelbue <mail@noelbuergler.ch>
2025-06-23 15:07:53 +0200

    Updated with new job

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,32 +1,42 @@
-import React from "react"
-import { useTranslation } from 'gatsby-plugin-react-i18next'
-import * as styles from "./AboutMe.module.css"
+import React from "react";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import * as styles from "./AboutMe.module.css";
 
 const AboutMe = ({ data }) => {
-    const { t, i18n } = useTranslation()
-    const content = data && data[i18n.language]
+  const { t, i18n } = useTranslation();
+  const content = data && data[i18n.language];
 
-    if (!content) {
-        return null; // or some fallback UI
-    }
+  if (!content) {
+    return null; // or some fallback UI
+  }
 
-    return (
-        <div className={styles.aboutMeWrapper}>
-            <h2 className={styles.sectionTitle}>{t('aboutMe')}</h2>
-            <div className={styles.aboutMe}>
-                <div className={styles.header}>
-                    <img src={content.profilePicture} alt={content.profileTitle} className={styles.profilePicture} />
-                    <h1 className={styles.title}>{content.profileTitle}</h1>
-                </div>
-                <p className={styles.description}>{content.profileDescription}</p>
-                <div className={styles.tags}>
-                    {content.profileTags.map((tag, index) => (
-                        <span key={index} className={styles.tag}>{tag}</span>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className={styles.aboutMeWrapper}>
+      <h2 className={styles.sectionTitle}>{t("aboutMe")}</h2>
+      <div className={styles.aboutMe}>
+        <div className={styles.header}>
+          <img
+            src={content.profilePicture}
+            alt={content.profileTitle}
+            className={styles.profilePicture}
+          />
+          <h1 className={styles.title}>{content.profileTitle}</h1>
         </div>
-    )
-}
+        <div className={styles.description}>
+          {content.profileDescription.split("\n").map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+        <div className={styles.tags}>
+          {content.profileTags.map((tag, index) => (
+            <span key={index} className={styles.tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/ResumeEntry.js
+++ src/components/ResumeEntry.js
@@ -1,41 +1,61 @@
-import React from "react"
-import { useTranslation } from 'gatsby-plugin-react-i18next'
-import * as styles from "./ResumeEntry.module.css"
+import React from "react";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import * as styles from "./ResumeEntry.module.css";
 
 const ResumeEntry = ({ data, type }) => {
-    const { t, i18n } = useTranslation()
-    const content = data[i18n.language]
+  const { t, i18n } = useTranslation();
+  const content = data[i18n.language];
 
-    if (!content) {
-        return null; // or some fallback UI
-    }
+  if (!content) {
+    return null; // or some fallback UI
+  }
 
-    return (
-        <section className={styles.resumeSection}>
-            <h2 className={styles.sectionTitle}>{t(type.toLowerCase())}</h2>
-            {content.map((entry, index) => (
-                <div key={index} className={styles.resumeItem}>
-                    <div className={styles.resumeHeader}>
-                        <a href={entry[`${type.toLowerCase()}LogoUrl`]} target="_blank" rel="noopener noreferrer">
-                            <img src={entry[`${type.toLowerCase()}Logo`]} alt={entry[`${type.toLowerCase()}Title`]} className={styles.logo} />
-                        </a>
-                        <div className={styles.resumeInfoTags}>
-                            {entry[`${type.toLowerCase()}InfoTags`].map((tag, tagIndex) => (
-                                <span key={tagIndex} className={styles.resumeInfoTag}>{tag}</span>
-                            ))}
-                        </div>
-                    </div>
-                    <h3 className={styles.resumeTitle}>{entry[`${type.toLowerCase()}Title`]}</h3>
-                    <p className={styles.resumeDescription}>{entry[`${type.toLowerCase()}Description`]}</p>
-                    <div className={styles.resumeTags}>
-                        {entry[`${type.toLowerCase()}Tags`].map((tag, tagIndex) => (
-                            <span key={tagIndex} className={styles.resumeTag}>{tag}</span>
-                        ))}
-                    </div>
-                </div>
+  return (
+    <section className={styles.resumeSection}>
+      <h2 className={styles.sectionTitle}>{t(type.toLowerCase())}</h2>
+      {content.map((entry, index) => (
+        <div key={index} className={styles.resumeItem}>
+          <div className={styles.resumeHeader}>
+            <a
+              href={entry[`${type.toLowerCase()}LogoUrl`]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={entry[`${type.toLowerCase()}Logo`]}
+                alt={entry[`${type.toLowerCase()}Title`]}
+                className={styles.logo}
+              />
+            </a>
+            <div className={styles.resumeInfoTags}>
+              {entry[`${type.toLowerCase()}InfoTags`].map((tag, tagIndex) => (
+                <span key={tagIndex} className={styles.resumeInfoTag}>
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+          <h3 className={styles.resumeTitle}>
+            {entry[`${type.toLowerCase()}Title`]}
+          </h3>
+          <div className={styles.resumeDescription}>
+            {entry[`${type.toLowerCase()}Description`]
+              .split("\n")
+              .map((line, index) => (
+                <p key={index}>{line}</p>
+              ))}
+          </div>
+          <div className={styles.resumeTags}>
+            {entry[`${type.toLowerCase()}Tags`].map((tag, tagIndex) => (
+              <span key={tagIndex} className={styles.resumeTag}>
+                {tag}
+              </span>
             ))}
-        </section>
-    )
-}
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default ResumeEntry
+export default ResumeEntry;
